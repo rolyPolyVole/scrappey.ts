879690df07da17a2af74618483274b8f7c1bd837
rolyPolyVole <yezhanting@gmail.com>
2024-07-12 21:38:26 +0100

    Improved type and intellisense.

--- src/types/scrappey.d.ts
+++ src/types/scrappey.d.ts
@@ -3,9 +3,29 @@ declare module "scrappey-wrapper-typed" {
         [key: string]: any
     }
 
+    /**
+     * Returns the string values of an enum
+     */
     type EnumValues<T extends KeyedObject> = 
         | T[keyof T] 
         | `${T[keyof T]}`
+
+    type IsDefined<T> = T extends undefined ? false : true;
+
+    /**
+     * Returns a boolean value indicating whether `K` is a key of type `T`
+     */
+    type HasDefinedKey<T, K extends keyof any> = K extends keyof T ? IsDefined<T[K]> : false;
+
+    /**
+     * Returns if an array `T` of objects contains any element with a key-value pair `K` and `V` 
+     */
+    type HasObjectWithKVRecord<T extends KeyedObject[], K extends keyof KeyedObject, V> = 
+        T extends [infer First, ...infer Rest] 
+            ? First extends Record<K, infer U> 
+                ? (U extends V ? true : HasObjectWithKVRecord<Rest extends KeyedObject[] ? Rest : never, K, V>)
+                : HasObjectWithKVRecord<Rest extends KeyedObject[] ? Rest : never, K, V>
+            : false;
 
     export type BrowserData = {
         name: string;
@@ -63,110 +83,6 @@ declare module "scrappey-wrapper-typed" {
         secure: boolean;
         sameSite: string;
     }
-
-    export type CreateSessionOptions = {
-        /**
-         * A custom session UUID to assign, defaults to a random UUID
-         */
-        session?: string;
-        /**
-         * A url to a proxy server, defaults to a random rotating proxy
-         */
-        proxy?: string;
-        /**
-         * Whitelists certain domains
-         */
-        whitelistedDomains?: string[];
-        /**
-         * If true, uses a datacenter proxy instead of a residential one
-         */
-        datacenter?: boolean;
-        /**
-         * Sets the browser name, min and max version
-         */
-        browser?: BrowserData;
-        /**
-         * Sets the OS, min and max version
-         */
-        operatingSystem?: OSOption | EnumValues<typeof OSOption>;
-        /**
-         * Specify whether the device is mobile or desktop
-         */
-        device?: DeviceOption | EnumValues<typeof DeviceOption>;
-    }
-
-    export enum APIEndpoint {
-        SessionCreate = "sessions.create",
-        SessionActive = "sessions.active",
-        SessionDestroy = "sessions.destroy",
-        RequestGet = "request.get",
-        RequestPost = "request.post"
-    }
-
-    export type RequestOptions = {
-        /**
-         * The command to send to the API
-         */
-        endpoint: APIEndpoint | EnumValues<typeof APIEndpoint>;
-        [key: string]: any;
-    }
-
-    export type BaseBrowserAction = {
-        when?: LoadType | EnumValues<typeof LoadType>
-    }
-
-    export type WaitableAction = {
-        /**
-         * The duration in seconds to wait after the action is completed
-         */
-        wait?: number;
-    }
-
-    export type ThrowableAction = {
-        /**
-         * Whether to ignore errors thrown by this action
-         */
-        ignoreErrors?: boolean;
-    }
-
-    export type ElementInteractionAction = BaseBrowserAction & ThrowableAction & WaitableAction & {
-        /**
-         * The [CSS selector](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_selectors) of the target element for this action
-         */
-        cssSelector: string;
-    }
-
-    export enum CatpchaType {
-        Custom = "custom",
-        Turnstile = "turnstile",
-        PerimeterX = "perimeterx",
-        ReCaptcha = "recaptcha",
-        HCaptcha = "hcaptcha",
-        MTCaptcha = "mtcaptcha"
-    }
-
-    export enum KeyboardAction {
-        Tab = "tab",
-        Enter = "enter",
-        Space = "space",
-        ArrowDown = "arrowdown",
-        ArrowUp = "arrowup",
-        ArrowLeft = "arrowleft",
-        ArrowRight = "arrowright"
-    }
-
-    export type BrowserAction = 
-        | ElementInteractionAction & { type: "click", waitForSelector?: boolean; }
-        | ElementInteractionAction & { type: "type", text: string }
-        | BaseBrowserAction & WaitableAction & { type: "discord_login", token: string }
-        | BaseBrowserAction & ThrowableAction & { type: "goto", url: string }
-        | Omit<ElementInteractionAction, "wait"> & { type: "wait_for_selector", timeout: number }
-        | WaitableAction & ThrowableAction & BaseBrowserAction & { type: "wait" }
-        | ThrowableAction & { type: "solve_captcha", captcha: CatpchaType | EnumValues<typeof CatpchaType> }
-        | ThrowableAction & ElementInteractionAction & { type: "solve_captcha", captcha: CatpchaType.Custom | "custom", inputSelector: string, clickSelector?: string }
-        | { type: "execute_js", code: string }
-        | ElementInteractionAction & { type: "scroll", repeat?: number, delayMs: number }
-        | { type: "keyboard", value: KeyboardAction | EnumValues<typeof KeyboardAction> }
 
     export enum ProxyCountry {
         UnitedStates = "UnitedStates",
@@ -298,7 +214,122 @@ declare module "scrappey-wrapper-typed" {
         Zambia = "Zambia"
     }
 
-    export type GetRequestOptions = {
+    export type CreateSessionOptions = {
+        /**
+         * A custom session UUID to assign, defaults to a random UUID
+         */
+        session?: string;
+        /**
+         * A url to a proxy server, defaults to a random rotating proxy
+         */
+        proxy?: string;
+        /**
+         * A specific country from where the rotating proxy should choose from
+         */
+        proxyCountry?: ProxyCountry | EnumValues<typeof ProxyCountry>;
+        /**
+         * Whitelists certain domains
+         */
+        whitelistedDomains?: string[];
+        /**
+         * If true, uses a datacenter proxy instead of a residential one
+         */
+        datacenter?: boolean;
+        /**
+         * Sets the browser name, min and max version
+         */
+        browser?: BrowserData;
+        /**
+         * Sets the OS, min and max version
+         */
+        operatingSystem?: OSOption | EnumValues<typeof OSOption>;
+        /**
+         * Specify whether the device is mobile or desktop
+         */
+        device?: DeviceOption | EnumValues<typeof DeviceOption>;
+        /**
+         * Whether to use a patched chrome browser, only applies if the browser is set to chrome (Firefox has this feature by default)
+         */
+        noDriver?: boolean;
+    }
+
+    export enum APIEndpoint {
+        SessionCreate = "sessions.create",
+        SessionActive = "sessions.active",
+        SessionDestroy = "sessions.destroy",
+        RequestGet = "request.get",
+        RequestPost = "request.post"
+    }
+
+    export type RequestOptions = {
+        /**
+         * The command to send to the API
+         */
+        endpoint: APIEndpoint | EnumValues<typeof APIEndpoint>;
+        [key: string]: any;
+    }
+
+    export type BaseBrowserAction = {
+        /**
+         * Whether this action should be executed before the supplied `url` loads, or after it
+         */
+        when?: LoadType | EnumValues<typeof LoadType>
+    }
+
+    export type WaitableAction = {
+        /**
+         * The duration in seconds to wait after the action is completed
+         */
+        wait?: number;
+    }
+
+    export type ThrowableAction = {
+        /**
+         * Whether to ignore errors thrown by this action
+         */
+        ignoreErrors?: boolean;
+    }
+
+    export type ElementInteractionAction = BaseBrowserAction & ThrowableAction & WaitableAction & {
+        /**
+         * The [CSS selector](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_selectors) of the target element for this action
+         */
+        cssSelector: string;
+    }
+
+    export enum CatpchaType {
+        Custom = "custom",
+        Turnstile = "turnstile",
+        PerimeterX = "perimeterx",
+        ReCaptcha = "recaptcha",
+        HCaptcha = "hcaptcha",
+        MTCaptcha = "mtcaptcha"
+    }
+
+    export enum KeyboardAction {
+        Tab = "tab",
+        Enter = "enter",
+        Space = "space",
+        ArrowDown = "arrowdown",
+        ArrowUp = "arrowup",
+        ArrowLeft = "arrowleft",
+        ArrowRight = "arrowright"
+    }
+
+    export type BrowserAction = 
+        | ElementInteractionAction & { type: "click", waitForSelector?: boolean; }
+        | ElementInteractionAction & { type: "type", text: string }
+        | BaseBrowserAction & WaitableAction & { type: "discord_login", token: string }
+        | BaseBrowserAction & ThrowableAction & { type: "goto", url: string }
+        | Omit<ElementInteractionAction, "wait"> & { type: "wait_for_selector", timeout: number }
+        | WaitableAction & ThrowableAction & BaseBrowserAction & { type: "wait" }
+        | ThrowableAction & { type: "solve_captcha", captcha: CatpchaType | EnumValues<typeof CatpchaType> }
+        | ThrowableAction & ElementInteractionAction & { type: "solve_captcha", captcha: CatpchaType.Custom | "custom", inputSelector: string, clickSelector?: string }
+        | { type: "execute_js", code: string }
+        | ElementInteractionAction & { type: "scroll", repeat?: number, delayMs: number }
+        | { type: "keyboard", value: KeyboardAction | EnumValues<typeof KeyboardAction> }
+
+    type GetRequestOptions = {
         /**
          * The page URL to navigate to
          */
@@ -319,17 +350,58 @@ declare module "scrappey-wrapper-typed" {
          * Custom request header data
          */
         customHeaders?: KeyedObject;
+        /**
+         * A specific country from which the rotating proxy should choose from
+         */
         proxyCountry?: ProxyCountry | EnumValues<typeof ProxyCountry>;
+        /**
+         * If true, response will contain a list of all image URLs on the website
+         */
         includeImages?: boolean;
+        /**
+         * If true, response will contaiin a list of all links on the website
+         */
         includeLinks?: boolean;
+        /**
+         * Takes `browser` or `request`
+         */
         requestType?: RequestType | EnumValues<typeof RequestType>;
+        /**
+         * Sets the local storage of the page
+         */
         localStorage?: KeyedObject;
+        /**
+         * Whether to add automatic mouse movements to reduce the chance of bot detection
+         */
         mouseMovements?: boolean;
+        /**
+         * Whether to automatically solve any captchas
+         */
         automaticallySolveCaptchas?: boolean;
+        /**
+         * If set to true, uses ChatGPT 3.5 to parse the page data using the query specified in `properties`
+         */
         autoparse?: boolean;
+        /**
+         * If `autoparse` is true, use this to specify the query to the AI
+         */
         properties?: string;
+        /**
+         * Whether to record a video of the actions performed on the page (A link will be returned in the response, under `videoUrl`)
+         */
         video?: boolean;
+        /**
+         * If true, takes a screenshot after all browser actions have been performed
+         */
+        screenshot?: boolean;
+        /**
+         * An ordered list of browser actions to be performed on the page, prioritising those with `when` set to `"beforeload"`
+         */
         browserActions?: BrowserAction[];
+        /**
+         * If the provided URL is directly to a PDF file or an image, returns that file in base64 format under `base64Response`
+         */
+        base64?: boolean;
         [key: string]: any;
     }
 
@@ -337,28 +409,93 @@ declare module "scrappey-wrapper-typed" {
         | Omit<GetRequestOptions, "customHeaders"> & { customHeaders: { content_type: "application/json" } } & { postData: KeyedObject } 
         | Omit<GetRequestOptions, "customHeaders"> & { customHeaders?: any } & { postData: string }
 
-    export type GetResponseData = {
+    type BaseGetResponseData = {
         solution: {
+            /**
+             * Whether the request was successful or not
+             */
             verified: boolean;
+            /**
+             * The current URL the request ended on
+             */
             currentUrl?: string;
+            /**
+             * The user agent string
+             */
             userAgent?: string;
+            /**
+             * The text content of the page
+             */
             innerText?: string;
+            /**
+             * The page's local storage data
+             */
             localStorageData?: {
                 [key: string]: string;
             }
+            /**
+             * A list of active cookies on the page
+             */
             cookies?: ActiveCookie[];
-            cookieString?: string;
+            /**
+             * The HTML content of the page
+             */
             response?: string;
-            type?: string;
-            javascriptReturn?: any[];
+            /**
+             * The request type
+             */
+            type?: RequestType | EnumValues<typeof RequestType>;
         }
-        videoUrl?: string;
+        /**
+         * The total amount of milliseconds it took for the request to complete
+         */
         timeElapsed: number;
+        /**
+         * Result state of the request, e.g. `"success"`, `"error"`
+         */
         data: string;
+        /**
+         * The session UUID used for this request
+         */
         session: string;
+        /**
+         * The request UUID
+         */
         request_uuid: string;
         [key: string]: any;
     }
+
+    type WithVideoURL = {
+        /**
+         * A link to a .webm file of the video recorded, only exists if `video` was set to true in the request 
+         */
+        videoUrl: string;
+    }
+
+    type WithJSOutput = Omit<BaseGetResponseData, "solution"> & {
+        solution: BaseGetResponseData["solution"] & {
+            /**
+             * A list of javascript outputs ordered in the same way as they were given in `browserActions`
+             * 
+             * Note: `when: "beforeload"` actions have priority 
+             */
+            javascriptReturn: any[];
+        }
+    }
+
+    // export type GetResponseData<R extends Partial<GetRequestOptions>> = 
+    //     HasDefinedKey<R, "video"> extends true
+    //         ? HasObjectWithKVRecord<R["browserActions"], "type", "execute_js"> extends true
+    //             ? (BaseGetResponseData & WithVideoURL) & WithJSOutput
+    //             : BaseGetResponseData & WithVideoURL
+    //         : HasObjectWithKVRecord<R["browserActions"], "type", "execute_js"> extends true
+    //             ? BaseGetResponseData & WithJSOutput
+    //             : BaseGetResponseData
+    
+    export type GetResponseData<R extends Partial<GetRequestOptions>> =
+        HasDefinedKey<R, "video"> extends true
+            ? (BaseGetResponseData & WithVideoURL) & WithJSOutput
+            : BaseGetResponseData & WithJSOutput
 
     export default class Scrappey {
         public readonly apiKey: string;
@@ -385,7 +522,7 @@ declare module "scrappey-wrapper-typed" {
          * @param data The request object
          * @returns {Promise<GetResponseData>} The reponse object
          */
-        public get(data: GetRequestOptions): Promise<GetResponseData>;
+        public get<R extends Partial<GetRequestOptions> & { url: string }>(data: R): Promise<GetResponseData<R>>;
 
         /**
          * Sends a POST request
